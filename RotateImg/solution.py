ee6fb8fdb02ed83d2770f1b2be009261ea0e9fbc
lucaslealvale <lucaslealvale01@gmail.com>
2022-09-19 20:29:34 -0300

    inversao matrix

--- RotateImg/solution.py
+++ RotateImg/solution.py
@@ -2,6 +2,7 @@ class Solution:
     def rotate(self, matrix: list[list[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
+        O ANDREW DEU SPOILER DESSA NA PRIMEIRA AULA
         """
         for i in range(len(matrix)):
             for j in range(i, len(matrix)):
